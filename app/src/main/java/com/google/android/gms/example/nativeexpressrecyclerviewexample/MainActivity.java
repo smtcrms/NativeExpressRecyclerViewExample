6e6ce02c344cf692c7e63581368402372f71a022
ramesh kumar <smtcrms@gmail.com>
2017-10-04 10:45:47 +0530

    Add files via upload

---
+++ app/src/main/java/com/google/android/gms/example/nativeexpressrecyclerviewexample/MainActivity.java
@@ -0,0 +1,216 @@
+package com.google.android.gms.example.nativeexpressrecyclerviewexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.NativeExpressAdView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple activity showing the use of {@link NativeExpressAdView} ads in
+ * a {@link RecyclerView} widget.
+ * <p>The {@link RecyclerView} widget is a more advanced and flexible version of
+ * ListView. This widget helps simplify the display and handling of large data sets
+ * by allowing the layout manager to determine when to reuse (recycle) item views that
+ * are no longer visible to the user. Recycling views improves performance by avoiding
+ * the creation of unnecessary views or performing expensive findViewByID() lookups.</p>
+ */
+public class MainActivity extends AppCompatActivity {
+
+    // A Native Express ad is placed in every nth position in the RecyclerView.
+    public static final int ITEMS_PER_AD = 8;
+
+    // The Native Express ad height.
+    private static final int NATIVE_EXPRESS_AD_HEIGHT = 150;
+
+    // The Native Express ad unit ID.
+    private static final String AD_UNIT_ID = "ca-app-pub-3836674649334184/2661924927";
+
+    // The RecyclerView that holds and displays Native Express ads and menu items.
+    private RecyclerView mRecyclerView;
+
+    // List of Native Express ads and MenuItems that populate the RecyclerView.
+    private List<Object> mRecyclerViewItems = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView.
+        mRecyclerView.setHasFixedSize(true);
+
+        // Specify a linear layout manager.
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        // Update the RecyclerView item's list with menu items and Native Express ads.
+        addMenuItemsFromJson();
+        addNativeExpressAds();
+        setUpAndLoadNativeExpressAds();
+
+        // Specify an adapter.
+        RecyclerView.Adapter adapter = new RecyclerViewAdapter(this, mRecyclerViewItems);
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    /**
+     * Adds Native Express ads to the items list.
+     */
+    private void addNativeExpressAds() {
+
+        // Loop through the items array and place a new Native Express ad in every ith position in
+        // the items List.
+        for (int i = 0; i <= mRecyclerViewItems.size(); i += ITEMS_PER_AD) {
+            final NativeExpressAdView adView = new NativeExpressAdView(MainActivity.this);
+            mRecyclerViewItems.add(i, adView);
+        }
+    }
+
+    /**
+     * Sets up and loads the Native Express ads.
+     */
+    private void setUpAndLoadNativeExpressAds() {
+        // Use a Runnable to ensure that the RecyclerView has been laid out before setting the
+        // ad size for the Native Express ad. This allows us to set the Native Express ad's
+        // width to match the full width of the RecyclerView.
+        mRecyclerView.post(new Runnable() {
+            @Override
+            public void run() {
+                final float scale = MainActivity.this.getResources().getDisplayMetrics().density;
+                // Set the ad size and ad unit ID for each Native Express ad in the items list.
+                for (int i = 0; i <= mRecyclerViewItems.size(); i += ITEMS_PER_AD) {
+                    final NativeExpressAdView adView =
+                            (NativeExpressAdView) mRecyclerViewItems.get(i);
+                    final CardView cardView = (CardView) findViewById(R.id.ad_card_view);
+                    final int adWidth = cardView.getWidth() - cardView.getPaddingLeft()
+                            - cardView.getPaddingRight();
+                    AdSize adSize = new AdSize((int) (adWidth / scale), NATIVE_EXPRESS_AD_HEIGHT);
+                    adView.setAdSize(adSize);
+                    adView.setAdUnitId(AD_UNIT_ID);
+                }
+
+                // Load the first Native Express ad in the items list.
+                loadNativeExpressAd(0);
+            }
+        });
+    }
+
+    /**
+     * Loads the Native Express ads in the items list.
+     */
+    private void loadNativeExpressAd(final int index) {
+
+        if (index >= mRecyclerViewItems.size()) {
+            return;
+        }
+
+        Object item = mRecyclerViewItems.get(index);
+        if (!(item instanceof NativeExpressAdView)) {
+            throw new ClassCastException("Expected item at index " + index + " to be a Native"
+                    + " Express ad.");
+        }
+
+        final NativeExpressAdView adView = (NativeExpressAdView) item;
+
+        // Set an AdListener on the NativeExpressAdView to wait for the previous Native Express ad
+        // to finish loading before loading the next ad in the items list.
+        adView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+                // The previous Native Express ad loaded successfully, call this method again to
+                // load the next ad in the items list.
+                loadNativeExpressAd(index + ITEMS_PER_AD);
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // The previous Native Express ad failed to load. Call this method again to load
+                // the next ad in the items list.
+                Log.e("MainActivity", "The previous Native Express ad failed to load. Attempting to"
+                        + " load the next Native Express ad in the items list.");
+                loadNativeExpressAd(index + ITEMS_PER_AD);
+            }
+        });
+
+        // Load the Native Express ad.
+        adView.loadAd(new AdRequest.Builder().build());
+    }
+
+    /**
+     * Adds {@link MenuItem}'s from a JSON file.
+     */
+    private void addMenuItemsFromJson() {
+        try {
+            String jsonDataString = readJsonDataFromFile();
+            JSONArray menuItemsJsonArray = new JSONArray(jsonDataString);
+
+            for (int i = 0; i < menuItemsJsonArray.length(); ++i) {
+
+                JSONObject menuItemObject = menuItemsJsonArray.getJSONObject(i);
+
+                String menuItemName = menuItemObject.getString("name");
+                String menuItemDescription = menuItemObject.getString("description");
+                String menuItemPrice = menuItemObject.getString("price");
+                String menuItemCategory = menuItemObject.getString("category");
+                String menuItemImageName = menuItemObject.getString("photo");
+
+                MenuItem menuItem = new MenuItem(menuItemName, menuItemDescription, menuItemPrice,
+                        menuItemCategory, menuItemImageName);
+                mRecyclerViewItems.add(menuItem);
+            }
+        } catch (IOException | JSONException exception) {
+            Log.e(MainActivity.class.getName(), "Unable to parse JSON file.", exception);
+        }
+    }
+
+    /**
+     * Reads the JSON file and converts the JSON data to a {@link String}.
+     *
+     * @return A {@link String} representation of the JSON data.
+     * @throws IOException if unable to read the JSON file.
+     */
+    private String readJsonDataFromFile() throws IOException {
+
+        InputStream inputStream = null;
+        StringBuilder builder = new StringBuilder();
+
+        try {
+            String jsonDataString = null;
+            inputStream = getResources().openRawResource(R.raw.menu_items_json);
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(inputStream, "UTF-8"));
+            while ((jsonDataString = bufferedReader.readLine()) != null) {
+                builder.append(jsonDataString);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+
+        return new String(builder);
+    }
+
+}
